cad83f43d8e7e156db051aa5fc2e245b199ccf05
Alex <david_alejandrorizo@outlook.com>
2024-12-12 11:11:52 -0600

    routes added

--- models/Server.js
+++ models/Server.js
@@ -6,6 +6,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
 
+import whatsappRoutes from '../routes/whatsapp.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -17,7 +19,7 @@ class Server {
         this.app = express();
         this.server = http.createServer(this.app);
         this.paths = {
-
+            whatsapp: '/api/whatsapp'
         };
 
         this.middlewares();
@@ -32,6 +34,8 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.paths.whatsapp, whatsappRoutes);
+        
         this.app.get('/*', (req, res) => {
             res.sendFile(path.join(__dirname, '../public', 'index.html'));
         });
